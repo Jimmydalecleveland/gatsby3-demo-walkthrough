87862cb71b4661297e96f9b676f7c5ce5b4bd912
Jimmy DC <jimmydalecleveland@gmail.com>
2021-10-01 11:36:13 -0600

    add random int util method

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import * as React from "react";
+import { getRandomIntInclusive } from "../utils";
+
+const rollD20 = getRandomIntInclusive(1, 20);
 
 export default function Component() {
-  return "Hello world";
+  return (
+    <div>
+      <h1>Title here</h1>
+      <h2>Roll was: {rollD20}</h2>
+    </div>
+  );
 }
